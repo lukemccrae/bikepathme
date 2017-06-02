f84b3c4db419a7d3a1bdcfaf095d84d0def3bd18
“lukemccrae” <lukemccrae@gmail.com>
2017-06-01 20:45:36 -0600

    ran into problem with cors and google maps api, dont know how to proceed

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+const COLUMNS = [
+  'carbohydrate_g',
+  'protein_g',
+  'fa_sat_g',
+  'fa_mono_g',
+  'fa_poly_g',
+  'kcal',
+  'description',
+];
+
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Fetch from 'react-fetch'
+import Request from 'superagent';
+
+const pathParams = {
+  a: 'a'
+}
+
 
 class App extends Component {
+
+  constructor(props) {
+  super(props);
+
+  this.state = {}
+}
+
+  componentWillMount() {
+    fetch('https://maps.googleapis.com/maps/api/directions/json?mode=bicycling&origin=38.8713700,-104.8221310&destination=38.9091780,-104.7828480&&waypoints=key=AIzaSyB6mjYhp5ca_RPpOdHu_Ul7E-YY6BYzmms')
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div className="container">
+          <h1 className="Header">Bike Path Me</h1>
+        <div>
+          <form>
+            <label>
+              How many destinations?
+              <select>
+                <option defaultValue value="0">0</option>
+                <option value="1-3">1-3</option>
+                <option value="4-5">4-5</option>
+                <option value="6+">6+</option>
+              </select>
+            </label>
+            <br></br>
+            <label>
+            What kind of places do you want to go to?
+              <select>
+                <option defaultValue value="all">All</option>
+                <option value="restaurants">Restaurants</option>
+                <option value="bars">Bars</option>
+                <option value="coffee shops">Coffee shops</option>
+              </select>
+            </label>
+            <br></br>
+            <input type="submit" value="Submit" />
+          </form>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
+
 }
 
 export default App;
